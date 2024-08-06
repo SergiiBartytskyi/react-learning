791605681088d1389334f4d119b1f1d4353ec2a9
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-08-06 17:43:30 +0200

    lesson 1-1-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import officers from "../../officers.json";
-import moduleName from "../SoldiersList/SoldiersList";
 import SoldiersList from "../SoldiersList/SoldiersList";
 
 export default function App() {
