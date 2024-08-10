a79b529cb810171cbdb8dbbdbbf1af2b3ec369bc
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-08-11 00:11:06 +0200

    add state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
-export default function App() {
-  return <></>;
-}
+// export default function App() {
+//   return (
+//     <>
+//       <CustomButton message="Playing music!">Play some music</CustomButton>
+//       <CustomButton message="Uploading your data!">Upload data</CustomButton>
+//     </>
+//   );
+// }
+
+const App = () => {
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
+  return (
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
+  );
+};
+
+export default App;
