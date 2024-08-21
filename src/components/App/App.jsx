d3f5dfdc31de32ef76e6056e11653e147690b98f
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-08-21 20:22:56 +0200

    add routing - 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,35 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-// import clsx from "clsx";
-import Home from "../../pages/Home";
-import About from "../../pages/About";
-import Products from "../../pages/Products";
-import ProductDetails from "../../pages/ProductDetails";
 import NotFound from "../../pages/NotFound";
 import { AppBar } from "../AppBar/AppBar";
-import { Mission } from "../Mission/Mission";
-import { Team } from "../Team/Team";
-import { Reviews } from "../Reviews/Reviews";
 import css from "./App.module.css";
+
+const Home = lazy(() => import("../../pages/Home"));
+const About = lazy(() => import("../../pages/About"));
+const ProductDetails = lazy(() => import("../../pages/ProductDetails"));
+const Products = lazy(() => import("../../pages/Products"));
+const Mission = lazy(() => import("../Mission/Mission"));
+const Team = lazy(() => import("../Team/Team"));
+const Reviews = lazy(() => import("../Reviews/Reviews"));
 
 const App = () => {
   return (
     <div className={css.container}>
       <AppBar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
